3c82f06a09c32e2a78a22158ea2dba033688d0bd
Zhyravskyy Oleh <zhyravskiy.o@gmail.com>
2017-11-27 14:13:34 +0200

    castom auth

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg
+ * Date: 26.11.17
+ * Time: 14:22
+ */
+
+namespace App\Http\Controllers\API;
+
+
+class UserController
+{
+
+}
---
+++ app/Http/Controllers/Clients.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg
+ * Date: 26.11.17
+ * Time: 12:45
+ */
+
+namespace App\Http\Controllers;
+
+
+class Clients
+{
+
+}
